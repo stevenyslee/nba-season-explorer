2442df70313b78297f22bbd904446272895b11b6
Steven Lee <stevenyslee@gmail.com>
2018-12-12 14:31:59 -0800

    Get CSV data from server

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Name from './name.jsx';
+import axios from 'axios';
 
 const elementsSymbol = [ "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt" ];
  
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('/nba/')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+  });
+  }
+
   render() {
     return (<div>
       <form>
--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const app = express();
-const csvFilePath= __dirname + '/../data/nba.csv';
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-  console.log(jsonObj);
-  /**
-   * [
-   *  {a:"1", b:"2", c:"3"},
-   *  {a:"4", b:"5". c:"6"}
-   * ]
-   */ 
-})
+const csv = require('csvtojson');
+
+const csvFilePath = __dirname + '/../data/nba.csv';
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/nba/', (req, res) => {
+  csv().fromFile(csvFilePath).then((jsonObj) => {
+    res.send(jsonObj);
+  });
+});
 
 app.listen(3000, () => console.log('App listening on port 3000'));
