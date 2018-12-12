7c28c72d0fd5c1128e9da7c9299c26fd006ebd77
Steven Lee <stevenyslee@gmail.com>
2018-12-12 14:25:41 -0800

    Read CSV file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
-
+const csvFilePath= __dirname + '/../data/nba.csv';
+const csv=require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+  console.log(jsonObj);
+  /**
+   * [
+   *  {a:"1", b:"2", c:"3"},
+   *  {a:"4", b:"5". c:"6"}
+   * ]
+   */ 
+})
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
