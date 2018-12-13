fca11a6698c408228e46ddc80c6d9c21f07dbdea
Steven Lee <stevenyslee@gmail.com>
2018-12-12 16:07:15 -0800

    Recieve formatted data

--- client/app.jsx
+++ client/app.jsx
@@ -6,27 +6,35 @@ import axios from 'axios';
 const elementsSymbol = [ "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt" ];
  
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   componentDidMount() {
     axios.get('/nba/')
     .then(function (response) {
       console.log(response);
+      this.setState({
+        data: response.data
+      });
     })
     .catch(function (error) {
       console.log(error);
-  });
+    });
   }
 
   render() {
-    return (<div>
-      <form>
-      <label>
-        <input type="text" name="name" />
-      </label>
-      </form>
-      <Name line={"Test1"} />
-      <Name line={"Test2"} />
-    </div>);
+    return (
+      <div>
+        <form>
+        <label>
+          <input type="text" name="name" />
+        </label>
+        </form>
+        <Name line={"Test1"} />
+        <Name line={"Test2"} />
+      </div>);
   }
 }
  
--- server/server.js
+++ server/server.js
@@ -8,7 +8,51 @@ app.use(express.static('public'));
 
 app.get('/nba/', (req, res) => {
   csv().fromFile(csvFilePath).then((jsonObj) => {
-    res.send(jsonObj);
+    let results = {};
+
+    jsonObj.forEach((record) => {
+      let homeTeam = record['Home/Neutral'];
+      let homePoints = record['PTS1'];
+      let visitorTeam = record['Visitor/Neutral'];
+      let visitorPoints =  record['PTS2'];
+      let date = record['Date'];
+
+      if (results[homeTeam]) {
+        results[homeTeam]['home'][date] = homePoints;
+        results[homeTeam]['both'][date] = homePoints;
+      } else {
+        results[homeTeam] = {
+          home: {
+            [date]: homePoints
+          },
+          visitor: {
+
+          },
+          both: {
+            [date]: homePoints
+          }
+        }
+      }
+
+      if (results[visitorTeam]) {
+        results[visitorTeam]['visitor'][date] = visitorPoints;
+        results[visitorTeam]['both'][date] = visitorPoints;
+      } else {
+        results[visitorTeam] = {
+          home: {
+
+          },
+          visitor: {
+            [date]: visitorPoints
+          },
+          both: {
+            [date]: visitorPoints
+          }
+        }
+      }
+    });
+    console.log(results);
+    res.send(results);
   });
 });
 
